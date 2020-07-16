75680c5d715035cac6f1ce61107a0cd921d64f35
Marco Aguila <marco.aguila@alumnos.udg.mx>
2020-07-16 17:01:26 -0500

    Ordenamiento de las colecciones de las respuestas

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -23,6 +23,7 @@ trait ApiResponser
             return $this->successResponse(['data' => $collection], $code);
         }
 
+        $collection = $this->sortData($collection);
         $transformer = $collection->first()->transformer;
         $collection = $this->transformData($collection, $transformer);
 
@@ -39,6 +40,17 @@ trait ApiResponser
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message ], $code);
+    }
+
+    protected function sortData(Collection $collection)
+    {
+        if (request()->has('sort_by')) {
+            $attribute = request()->sort_by;
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+
+        return $collection;
     }
 
     protected function transformData($data, $transformer) 
