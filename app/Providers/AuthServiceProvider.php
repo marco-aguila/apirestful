53184ee210983f23e26bc8f2a14ae44e63815f77
Marco Aguila <marco.aguila@alumnos.udg.mx>
2020-07-22 14:33:24 -0500

    tiempo de experacion de tokens

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,5 +27,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->addMinutes(30));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
